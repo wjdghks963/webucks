b36e3c809a9fd7bcd8058ddf5db5d682cb8d2632
최정환 <junghwan@jung.local>
2022-02-21 15:56:46 +0900

    mission 2

---
+++ index.js
@@ -0,0 +1,21 @@
+const loginId = document.querySelector("#email");
+const loginPasswrod = document.querySelector("#password");
+const loginBtn = document.querySelector(".loginBtn");
+
+let idValue;
+let passwordValue;
+
+const checkValue = (e) =>{
+    if(e.target.id == "email"){
+        idValue = e.target.value
+    }else if(e.target.id == "password"){
+        passwordValue = e.target.value;
+    }
+    if(idValue.includes("@") && passwordValue.length > 7){
+        loginBtn.style.opacity = 1;
+    }else{
+        loginBtn.style.opacity = 0.3;
+    }
+}
+loginId.addEventListener("input", checkValue);
+loginPasswrod.addEventListener("input",checkValue)
